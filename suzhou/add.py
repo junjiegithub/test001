5e9d334308a99f4c03b753efe4a12b0a8b24d35b
junjie <14>
2020-04-27 08:29:52 +0800

    编写接口

--- suzhou/add.py
+++ suzhou/add.py
@@ -16,7 +16,7 @@ def sub():
     b= request.json.get("b")
     result =str(float(a)-float(b))#使用float支持浮点数相减
     #使用jsonify将字典数据转换为json类型的相应数据
-    return jsonify({"code":"100000","msg":"成功","data":result})
+    return jsonify({"code":"100000","msg1":"成功","data":result})
 
 if __name__ == '__main__':
     app.run()

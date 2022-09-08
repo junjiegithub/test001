33503617f82b8280d50a6f301d7ac3c438a8da53
wujunjie <141627053@qq.com>
2022-09-08 21:54:07 +0800

    输入

---
+++ 190917/输入.py
@@ -0,0 +1,14 @@
+"""
+1。书写input
+    input（"提示信息"）
+
+2。观察特点
+    2.1 遇到input，等待用户输入
+    2.2 接收input存变量
+    2.3input接收的数据类型都是字符串
+
+"""
+
+password=input('请输入你的密码')
+print(f"您的密码是{password}")
+print(type(password))
